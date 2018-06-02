24ccb28e4fbeb03e645017d3a4d616cda9e030af
Lisa Mac <lisamac421@gmail.com>
2018-06-02 15:48:27 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -21,7 +21,7 @@ function pressIt() {
 
 function submitIt() {
   $('form').on('submit', function() {
-    alerts("Your form is going to be submitted now.")
+    alert("Your form is going to be submitted now.")
   });
 }
 

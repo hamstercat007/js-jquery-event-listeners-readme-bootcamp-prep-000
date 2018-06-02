cf3c8187376b562bc6a602e7de918c5903aff284
Lisa Mac <lisamac421@gmail.com>
2018-06-02 15:20:08 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,13 +1,11 @@
 //define functions here
+function getIt() {
+  $('p').on('click', function() {
+    alert("Hey!");
+  });
+}
 
 $(document).ready(function(){
-
-// call functions here
-function getIt() {
-    $('p').on('click', function() {
-      alert("Hey!");
-    });
-  }
+  // call functions here
   getIt();
 });
-
